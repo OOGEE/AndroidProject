ca4c73aead92fee8708379002ebc5b4163d77820
O <familye@naver.com>
2019-11-06 17:41:22 +0900

    AP

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent myintent = new Intent(getApplicationContext(), menu.class);
+        startActivity(myintent);
+    }
+}
